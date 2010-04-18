9c736420f198e671c221066359111441f0909bd0
Radim Novotny <novotny.radim@gmail.com>
2010-04-18 09:05:12 +0000

    * Fixed order in portal_javascripts * Fixed image names for IE * added z3c autoinclude plugin

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.4'
+version = '0.4.1'
 
 setup(name='collective.fancybox',
       version=version,
@@ -29,5 +29,8 @@ setup(name='collective.fancybox',
       ],
       entry_points="""
       # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
       """,
       )
