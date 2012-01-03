6e6f214c51576aa708a2e2fcf99c16c34cc23225
JeanMichel FRANCOIS <toutpt@gmail.com>
2012-01-03 16:11:58 +0100

    rename collective.fancybox to collective.js.fancybox

--- setup.py
+++ setup.py
@@ -1,26 +1,28 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.5.1'
+version = '1.3.4.1dev'
 
-setup(name='collective.fancybox',
+setup(name='collective.js.fancybox',
       version=version,
       description="Fancybox libraries for Plone",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
+        "Framework :: Plone :: 4.0",
+        "Framework :: Plone :: 4.1",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='fancybox plone jquery',
       author='Radim Novotny',
       author_email='novotny.radim@gmail.com',
-      url='http://pypi.python.org/pypi/collective.fancybox',
+      url='http://pypi.python.org/pypi/collective.js.fancybox',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['collective'],
+      namespace_packages=['collective','collective.js'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
@@ -29,7 +31,6 @@ setup(name='collective.fancybox',
       ],
       entry_points="""
       # -*- Entry points: -*-
-
       [z3c.autoinclude.plugin]
       target = plone
       """,
