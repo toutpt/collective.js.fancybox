d94a6af012c9628433543629bda5bbf127435ffa
JeanMichel FRANCOIS <toutpt@gmail.com>
2012-01-04 11:21:20 +0100

    rename resource form collective.fancybox to collective.js.fancybox on Radim advice

--- collective/js/fancybox/upgrades.py
+++ collective/js/fancybox/upgrades.py
@@ -2,11 +2,18 @@ from Products.CMFCore.utils import getToolByName
 
 def upgrade_to_1000(context):
     cssregistry = getToolByName(context, 'portal_css')
+    jsregistry = getToolByName(context, 'portal_javascripts')
     setup = getToolByName(context, 'portal_setup')
     
     cssregistry.unregisterResource('jquery.fancybox.css')
+    cssregistry.unregisterResource('++resource++collective.fancybox/jquery.fancybox.css')
+    jsregistry.unregisterResource('++resource++collective.fancybox/jquery.fancybox.pack.js')
     
     setup.runImportStepFromProfile('profile-collective.js.fancybox:default',
                                    'cssregistry',
+                                   run_dependencies=False,
+                                   purge_old=False)
+    setup.runImportStepFromProfile('profile-collective.js.fancybox:default',
+                                   'jsregistry',
                                    run_dependencies=False,
                                    purge_old=False)
